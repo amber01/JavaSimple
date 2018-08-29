1bb581e5baa467bdcb16e08d142b1a3ce42bff5b
ShlityWang <shlidty@163.com>
2018-08-29 19:08:04 +0800

    bug fix

---
+++ demo1/demo/src/main/java/com/example/demo/conf/GlobalDefaultExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.demo.conf;
+
+/**
+ * 1. 新建一个雷GloablDefaultExceptionHandler
+ * 2. 在class注解上@ControllerAdvice
+ * 3. 在方法上注解上@ExceptionHandler(value = Exception.class)，具体代码如下：
+ *
+ *
+ * @ Author     ：tank
+ * @ Date       ：Created in 18:59 2018/8/29
+ * @ Description：
+ * @ Modified By：
+ * @Version:
+ */
+public class GlobalDefaultExceptionHandler {
+
+}
