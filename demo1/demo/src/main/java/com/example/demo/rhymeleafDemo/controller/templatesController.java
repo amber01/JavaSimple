f4b79f4649290833bbdb0707d162ff381d27c952
ShlityWang <shlidty@163.com>
2018-08-29 23:56:23 +0800

    bug fix

--- demo1/demo/src/main/java/com/example/demo/rhymeleafDemo/controller/templatesController.java
+++ demo1/demo/src/main/java/com/example/demo/rhymeleafDemo/controller/templatesController.java
@@ -1,4 +1,14 @@
 package com.example.demo.rhymeleafDemo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/templates")  //这里对应sources目录下的templates目录
 public class templatesController {
+
+    @RequestMapping("/hello")
+    public  String hello(){
+        return "personView";  //这里返回对应的HTML名称
+    }
 }
