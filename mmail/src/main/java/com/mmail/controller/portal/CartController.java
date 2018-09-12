9b553f5b0959c73e222e65d5e89c98355c76d48b
ShlityWang <shlidty@163.com>
2018-09-12 19:05:47 +0800

    bug fix

--- mmail/src/main/java/com/mmail/controller/portal/CartController.java
+++ mmail/src/main/java/com/mmail/controller/portal/CartController.java
@@ -7,6 +7,8 @@ import com.mmail.pojo.User;
 import com.mmail.service.ICartService;
 import com.mmail.vo.CartVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +29,7 @@ public class CartController {
     private ICartService iCartService;
 
     //添加商品到购物车
-    @RequestMapping("add.do")
+    @PostMapping("add.do")
     public ServerResponse<CartVo> add(HttpSession session, Integer count, Integer productId){
         User user = (User) session.getAttribute(Const.CURRENT_USER);
         if (user == null){
@@ -37,7 +39,7 @@ public class CartController {
     }
 
     //购物车更新修改
-    @RequestMapping("update.do")
+    @PostMapping("update.do")
     public ServerResponse<CartVo> update(HttpSession session, Integer count, Integer productId){
         User user = (User) session.getAttribute(Const.CURRENT_USER);
         if (user == null){
@@ -47,7 +49,7 @@ public class CartController {
     }
 
     //购物车删除商品
-    @RequestMapping("delete.do")
+    @PostMapping("delete.do")
     public ServerResponse<CartVo> delete(HttpSession session, String productIds){
         User user = (User) session.getAttribute(Const.CURRENT_USER);
         if (user == null){
@@ -57,7 +59,7 @@ public class CartController {
     }
 
     //获取购物车列表
-    @RequestMapping("list.do")
+    @GetMapping("list.do")
     public ServerResponse<CartVo> list(HttpSession session){
         User user = (User) session.getAttribute(Const.CURRENT_USER);
         if (user == null){
